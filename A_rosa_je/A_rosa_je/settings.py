76e681955ef0262a4f5fd25ea1ca8b2604241baa
RemiAgeron <remiageron@gmail.com>
2023-02-02 19:31:48 +0100

    Add Swagger

--- A_rosa_je/A_rosa_je/settings.py
+++ A_rosa_je/A_rosa_je/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
--- A_rosa_je/A_rosa_je/urls.py
+++ A_rosa_je/A_rosa_je/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
-urlpatterns = [
+from rest_framework_swagger.views import get_swagger_view
+
+urlpatterns =   [
     path('admin/', admin.site.urls),
+    re_path('docs/', get_swagger_view(title="A'Rosa-je"))
 ]
