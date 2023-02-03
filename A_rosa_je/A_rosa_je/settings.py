376932f7d3662cf371b15eeb4a11d70187cee676
RemiAgeron <remiageron@gmail.com>
2023-02-03 12:30:31 +0100

    Add app_site

--- A_rosa_je/A_rosa_je/settings.py
+++ A_rosa_je/A_rosa_je/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_swagger'
+    'rest_framework_swagger',
+    'A_rosa_je_site'
 ]
 
 MIDDLEWARE = [
