a68c178af45614a4e07efcf427146b44471b8424
RemiAgeron <remiageron@gmail.com>
2023-02-14 21:22:35 +0100

    Add rest API & Swagger

--- A_rosa_je/urls.py
+++ A_rosa_je/urls.py
@@ -1,8 +1,9 @@
 """A_rosa_je URL Configuration"""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns =   [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/', include('A_rosa_je_site.urls'))
 ]
