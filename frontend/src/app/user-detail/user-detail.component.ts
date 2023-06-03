09d68dcf5984b2794ea97bfd6b4ff8cc00e9ddcd
RemiAgeron <remiageron@gmail.com>
2023-06-03 23:51:06 +0200

    BugFix user-detail

--- frontend/src/app/user-detail/user-detail.component.ts
+++ frontend/src/app/user-detail/user-detail.component.ts
@@ -35,40 +35,19 @@ export class UserDetailComponent {
     firstname: "Dimitry",
     lastname: "Neutron",
     biography: "Voici une petite description de qui je suis...",
-    publications: [
-      {
-        description: "Belle plante",
-        plant: "Avocatier"
-      },
-      {
-        description: "Belle plante",
-        plant: "Avocatier"
-      },
-      {
-        description: "Belle plante",
-        plant: "Avocatier"
-      },
-      {
-        description: "Belle plante",
-        plant: "Avocatier"
-      },
-      {
-        description: "Belle plante",
-        plant: "Avocatier"
-      },
-    ],
-    reviews: [
-      {
-        user: {id: 1, firstname: "Rémi", lastname: "Ageron", biography: "Voici ma description...", publications: [], reviews: []},
-        message: "Personnes de confiance, donne de très bons conseils."
-      }
-    ]
+    publications: new Array(28).fill({
+      description: "Belle plante",
+      plant: "Avocatier"
+    }),
+    reviews: new Array(20).fill({
+      user: {id: 1, firstname: "Rémi", lastname: "Ageron", biography: "Voici ma description...", publications: [], reviews: []},
+      message: "Personnes de confiance, donne de très bons conseils."
+    })
   }
 
   public segmentChoice: string = "publication";
 
   handleChange(event: any) {
     this.segmentChoice = event.target.value;
-    console.log(this.segmentChoice);
   }
 }
